4c85249ee768eb372248e07d799c91b59c32c288
amine hadef <amine.hadef.dev@gmail.com>
2025-12-16 19:23:36 +0100

    fix: form class to classname

--- assets/react/controllers/Beneficiary.jsx
+++ assets/react/controllers/Beneficiary.jsx
@@ -2,9 +2,9 @@ import React from "react"
 
 export default function Beneficiary({ beneficiary }) {
     return (
-        <div class="w-48 max-w-sm rounded overflow-hidden shadow-lg flex flex-col items-center text-center">
-            <img class="w-full" src={beneficiary.avatarUrl} alt="Sunset in the mountains" />
-            <div class="px-6 py-4">
+        <div className="w-48 max-w-sm rounded overflow-hidden shadow-lg flex flex-col items-center text-center">
+            <img className="w-full" src={beneficiary.avatarUrl} alt="Sunset in the mountains" />
+            <div className="px-6 py-4">
                 <a href={"https://localhost:8000/api/beneficiaries/" + beneficiary.id} ><p className="text-gray-700 text-base text-white">{beneficiary.name}</p></a>
             </div>
         </div>
